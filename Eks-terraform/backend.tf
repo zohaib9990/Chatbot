a988d2d743794161656d02f5a020a21ab04ae9e8
Ubuntu <ubuntu@ip-172-31-43-141.ec2.internal>
2025-04-03 20:36:28 +0000

    Updated project files

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "temp-repo"  # Ensure this bucket exists
+    bucket = "temp-repo-23"  # Ensure this bucket exists
     key    = "EKS/terraform.tfstate"
-    region = "eu-central-1"  # Update to match actual bucket region
+    region = "us-east-1"  # Update to match actual bucket region
   }
 }
 
--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -9,5 +9,5 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-central-1"  # Change to the correct region
+  region = "us-east-1"  # Change to the correct region
 }
